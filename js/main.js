93a35cdff855a885572c26fef363d8a4261abdee
Dmytro-Lysenko <65546160+Dmytro-Lysenko@users.noreply.github.com>
2021-10-22 10:45:42 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -37,6 +37,9 @@ openIcon.addEventListener('click', ()=> {
     mobileNav.classList.remove('slide-out');
     mobileNav.classList.toggle('slide-in');
     openIcon.classList.toggle('display-none');
+    setTimeout(() => {
+        closeIcon.classList.toggle('display-none');
+    },500);
 });
 
 closeIcon.addEventListener('click', () => {
